3aad9888b95c6c8b691c4010eaaedae638061904
iztiar-0 <iztiar-leader@trychlos.org>
2022-03-24 09:10:41 +0100

    Take advantage of IFeatureProvider.vFeatureInitialized() and validate add a verb to ITcpServer

--- src/plugin.class.js
+++ src/plugin.class.js
@@ -28,14 +28,11 @@ export class pidUsagePlugin {
 
         // must implement the IFeatureProvider
         //  should implement that first so that we can install the engineApi and the featureCard as soon as possible
-        Interface.add( this, exports.IFeatureProvider );
+        Interface.add( this, exports.IFeatureProvider, {
+            vFeatureInitialized: this.ifeatureproviderFeatureInitialized
+        });
         this.IFeatureProvider.api( api );
         this.IFeatureProvider.feature( card );
-
-        // add a status part
-        exports.IStatus.add( instance, this.statusPart, this );
-        // add a capability
-        exports.ICapability.add( instance, 'pidUsage', this.pidUsage );
 
         let _promise = this._fillConfig()
             .then(() => { return Promise.resolve( this ); });
@@ -57,20 +54,40 @@ export class pidUsagePlugin {
         return this.IFeatureProvider.fillConfig( _filled ).then(( c ) => { return feature.config( c ); });
     }
 
+    /*
+     * Called on each and every loaded add-on when the main hosting feature has terminated with its initialization
+     * Time, for example, to increment all interfaces we are now sure they are actually implemented
+     * note:
+     *  we have to wait until the end of main feature initialization to be sure that all interfaces are actually
+     *  implemented - even if IStatus and ICapability are tolerant an able to auto-implement, ITcpServer requires
+     *  that the implementation provide a configuration and so cannot be auto-implemented.
+     */
+    ifeatureproviderFeatureInitialized(){
+        const exports = this.IFeatureProvider.api().exports();
+        exports.Msg.debug( 'pidUsagePlugin.ifeatureproviderInitialized()', this.IFeatureProvider.feature().name());
+
+        // add a status part
+        //  this is the feature this plugin is planned for
+        exports.IStatus.add( this._instance, this.statusPart, this );
+
+        // some other POCs - all tested OK, but unneeded here
+        //  add a capability
+        //exports.ICapability.add( this._instance, 'pidUsage', this.pidUsage );
+        // add a tcp command + args
+        //exports.ITcpServer.add( this._instance, 'pidUsage', 'Display the resources usage', this.displayUsage, false, 'another', 'arg' );
+    }
+
+    // TCP Server command
     // @param {Object} instance the implementation instance
     // @param {pidUsagePlugin} self this instance
     // @returns {Promise} which resolves to the PID usage
-    statusPart( instance, self ){
+    displayUsage( instance, reply ){
         const exports = instance.IFeatureProvider.api().exports();
-        exports.Msg.debug( 'pidUsagePlugin.statusPart()', self.IFeatureProvider.feature().name());
-        return self.pidUsage( instance, 'cap', self )
-            .then(( res ) => {
-                const name = self.IFeatureProvider.feature().name().split( '/' )[1];
-                let o = {};
-                o[name] = res;
-                //exports.Msg.debug( 'pidUsagePlugin.statusPart()', 'pidUsage', o );
-                return Promise.resolve( o );
-            });
+        let _args = [ ...arguments ];
+        _args.splice( 0, 2 );
+        reply.answer = _args;
+        exports.Msg.debug( 'pidUsagePlugin.displayUsage() replying', reply );
+        return Promise.resolve( reply );
     }
 
     /*
@@ -87,6 +104,22 @@ export class pidUsagePlugin {
                     elapsed: res.elapsed
                 };
                 exports.Msg.debug( 'pidUsagePlugin.pidUsage()', o );
+                return Promise.resolve( o );
+            });
+    }
+
+    // @param {Object} instance the implementation instance
+    // @param {pidUsagePlugin} self this instance
+    // @returns {Promise} which resolves to the PID usage
+    statusPart( instance, self ){
+        const exports = instance.IFeatureProvider.api().exports();
+        exports.Msg.debug( 'pidUsagePlugin.statusPart()', self.IFeatureProvider.feature().name());
+        return self.pidUsage( instance, 'cap', self )
+            .then(( res ) => {
+                const name = self.IFeatureProvider.feature().name().split( '/' )[1];
+                let o = {};
+                o[name] = res;
+                //exports.Msg.debug( 'pidUsagePlugin.statusPart()', 'pidUsage', o );
                 return Promise.resolve( o );
             });
     }
